fc109f72edd861aee1025894cba29f9dfb9946ad
shiv2398 <sahityahavi111@gmail.com>
2023-12-26 01:17:21 +0530

    .

--- dc_gan.py
+++ dc_gan.py
@@ -1,33 +1,41 @@
-class Generator(nn.Module):
-  def __init__(self):
-      super().__init__()
-      self.model = nn.Sequential(
-      nn.Linear(100, 256),
-      nn.LeakyReLU(0.2),
-      nn.Linear(256, 512),
-      nn.LeakyReLU(0.2),
-      nn.Linear(512, 1024),
-      nn.LeakyReLU(0.2),
-      nn.Linear(1024, 784),
-      nn.Tanh()
-      )
-  def forward(self, x):
-    return self.model(x)
 class Discriminator(nn.Module):
-  def __init__(self):
-        super().__init__()
+    def __init__(self):
+        super(Discriminator, self).__init__()
         self.model = nn.Sequential(
-        nn.Linear(784, 1024),
-        nn.LeakyReLU(0.2),
-        nn.Dropout(0.3),
-        nn.Linear(1024, 512),
-        nn.LeakyReLU(0.2),
-        nn.Dropout(0.3),
-        nn.Linear(512, 256),
-        nn.LeakyReLU(0.2),
-        nn.Dropout(0.3),
-        nn.Linear(256, 1),
-        nn.Sigmoid()
+            nn.Conv2d(3,64,4,2,1,bias=False),
+            nn.LeakyReLU(0.2,inplace=True),
+            nn.Conv2d(64,64*2,4,2,1,bias=False),
+            nn.BatchNorm2d(64*2),
+            nn.LeakyReLU(0.2,inplace=True),
+            nn.Conv2d(64*2,64*4,4,2,1,bias=False),
+            nn.BatchNorm2d(64*4),
+            nn.LeakyReLU(0.2,inplace=True),
+            nn.Conv2d(64*4,64*8,4,2,1,bias=False),
+            nn.BatchNorm2d(64*8),
+            nn.LeakyReLU(0.2,inplace=True),
+            nn.Conv2d(64*8,1,4,1,0,bias=False),
+            nn.Sigmoid()
         )
-  def forward(self, x):
-     return self.model(x)
+        self.apply(weights_init)
+    def forward(self, input): return self.model(input)
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator,self).__init__()
+        self.model = nn.Sequential(
+            nn.ConvTranspose2d(100,64*8,4,1,0,bias=False,),
+            nn.BatchNorm2d(64*8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64*8,64*4,4,2,1,bias=False),
+            nn.BatchNorm2d(64*4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d( 64*4,64*2,4,2,1,bias=False),
+            nn.BatchNorm2d(64*2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d( 64*2,64,4,2,1,bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d( 64,3,4,2,1,bias=False),
+            nn.Tanh()
+        )
+        self.apply(weights_init)
+    def forward(self,input): return self.model(input)
